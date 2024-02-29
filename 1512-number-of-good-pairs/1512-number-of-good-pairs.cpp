56d8d8beace3e736ab8444a4ebfb1057b8acb74f
tarun-1910 <119746382+tarun-1910@users.noreply.github.com>
2024-02-29 20:59:56 +0530

    Time: 4 ms (27.57%), Space: 9.1 MB (9.88%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+      unordered_map<int, int> freq;
+        int count = 0;
+        for (int num : nums) {
+            count += freq[num];
+            freq[num]++;
+        }
+        return count;
+    }
+};
