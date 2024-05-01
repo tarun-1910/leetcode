d85ded735078e4b7b1f52c6fa71a84b28d2c9d3e
tarun-1910 <119746382+tarun-1910@users.noreply.github.com>
2024-05-01 18:36:09 +0530

    Time: 6 ms (67.60%), Space: 34 MB (85.69%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        int sum1=0,sum2=0;
+        for(int i=0;i<nums1.size();i++){
+            sum1=sum1+nums1[i];
+        }
+        for(int i=0;i<nums2.size();i++){
+            sum2=sum2+nums2[i];
+        }
+        cout<<sum1<<" "<<sum2<<endl;
+        
+        int res=0;
+        int siz=nums1.size();
+        res=(sum1-sum2)/siz;
+        cout<<res<<endl;
+        
+        
+        return -res;
+    
+    }
+};
